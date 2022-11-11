af8533ef17af917327b17c7cfb3d2456eb691748
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-12 00:22:12 +0530

    Prime Factors

---
+++ PrimeFactors/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PrimeFactors
+{
+    public class program
+    {
+
+        public static void primeFactors(int n)
+        {
+            while (n % 2 == 0)
+            {
+                Console.Write(2 + " ");
+                n /= 2;
+            }
+
+            for (int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                while (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                    n /= i;
+                }
+            }
+
+            if (n > 2)
+                Console.Write(n);
+        }
+
+        public static void Main()
+        {
+            int n = 315;
+            primeFactors(n);
+        }
+    }
+}
